5d052722085c6dd71efa66e85677ed1d1c29a5cb
hchesnutt <t.henry.chesnutt@gmail.com>
2018-03-21 20:52:59 -0700

    fixed video player

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,11 +1,11 @@
-var VideoPlayer = () => (
-  <div className="video-player">
+var VideoPlayer = (props) => (
+<div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={`https://www.youtube.com/embed/${props.video.id.videoId}`} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3 className="video-player-title">{props.video.snippet.title}</h3>
+      <div className="video-player-description">{props.video.snippet.title}</div>
     </div>
   </div>
 );
